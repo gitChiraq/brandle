8914dc5ea45a9c828b481936990d27cd4c9c51e7
gitChiraq <mbrinkmann04@gmail.com>
2024-04-10 21:03:39 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <GameBoard />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import GameBoard from './components/GameBoard.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { GameBoard },
+  data(){
+    return{
+      title: 'brandle'
+    }
   }
 }
 </script>
@@ -20,7 +24,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ABB2B9;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/GameBoard.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="row">
+    <div class="element1"></div>
+    <div class="element2"></div>
+    <div class="element3"></div>
+    <div class="element4"></div>
+    <div class="element5"></div>
+  </div>
+  <div class="row">
+    <div class="element1"></div>
+    <div class="element2"></div>
+    <div class="element3"></div>
+    <div class="element4"></div>
+    <div class="element5"></div>
+  </div>
+  <div class="row">
+    <div class="element1"></div>
+    <div class="element2"></div>
+    <div class="element3"></div>
+    <div class="element4"></div>
+    <div class="element5"></div>
+  </div>
+  <div class="row">
+    <div class="element1"></div>
+    <div class="element2"></div>
+    <div class="element3"></div>
+    <div class="element4"></div>
+    <div class="element5"></div>
+  </div>
+  <div class="row">
+    <div class="element1"></div>
+    <div class="element2"></div>
+    <div class="element3"></div>
+    <div class="element4"></div>
+    <div class="element5"></div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.row{
+    height: 48px;
+    width: 400px;
+    
+    margin: 0 auto;
+
+}
+
+.element1{
+    display: inline-block;
+    height: 40px;
+    width: 50px;
+    margin: 2px;
+    border:1px solid grey;
+}
+.element2{
+    display:inline-block;
+    height: 40px;
+    width: 50px;
+    margin: 2px;
+    border:1px solid grey;
+}
+.element3{
+    display:inline-block;
+    height: 40px;
+    width: 50px;
+    margin: 2px;
+    border:1px solid grey;
+}
+.element4{
+    display:inline-block;
+    height: 40px;
+    width: 50px;
+    margin: 2px;
+    border:1px solid grey;
+}
+.element5{
+    display:inline-block;
+    height: 40px;
+    width: 50px;
+    margin: 2px;
+    border:1px solid grey;
+}
+
+</style>
